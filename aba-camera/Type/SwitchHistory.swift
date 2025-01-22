4a0eaedba238b338663570822de6dfaa3f93dfa9
t-asa-nagaoka <165289529+t-asa-nagaoka@users.noreply.github.com>
2025-01-22 11:47:15 +0900

    add SwitchHistoryExtractStatus

--- aba-camera/Type/SwitchHistory.swift
+++ aba-camera/Type/SwitchHistory.swift
@@ -7,14 +7,22 @@
 
 import Foundation
 
+enum SwitchHistoryExtractStatus: Int {
+    case none = 0
+    case success = 1
+    case failed = 2
+}
+
 struct SwitchHistory : SwitchHistoryProtocol {
     var id: Int
     var happend: Date
     var subjective: Bool
+    var status: SwitchHistoryExtractStatus
     
     init(id: Int, happend: Date, subjective: Bool) {
         self.id = id
         self.happend = happend
         self.subjective = subjective
+        self.status = .none
     }
 }
